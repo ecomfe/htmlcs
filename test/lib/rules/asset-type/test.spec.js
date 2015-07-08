096fa81ded76ec39e4c8f117ab405f98a406138a
nighca <nighca@live.cn>
2015-07-08 12:20:26 +0800

    add <style> check & format in rule asset-type

--- test/lib/rules/asset-type/test.spec.js
+++ test/lib/rules/asset-type/test.spec.js
@@ -13,7 +13,7 @@ describe('hint rule ' + rule, function () {
     var result = htmlcs.hintFile(path.join(__dirname, 'case.html'));
 
     it('should return right result', function () {
-        expect(result.length).toBe(2);
+        expect(result.length).toBe(3);
 
         expect(result[0].type).toBe('WARN');
         expect(result[0].code).toBe('001');
@@ -21,9 +21,14 @@ describe('hint rule ' + rule, function () {
         expect(result[0].col).toBe(5);
 
         expect(result[1].type).toBe('WARN');
-        expect(result[1].code).toBe('002');
-        expect(result[1].line).toBe(14);
+        expect(result[1].code).toBe('001');
+        expect(result[1].line).toBe(13);
         expect(result[1].col).toBe(5);
+
+        expect(result[2].type).toBe('WARN');
+        expect(result[2].code).toBe('002');
+        expect(result[2].line).toBe(16);
+        expect(result[2].col).toBe(5);
     });
 });
 
@@ -33,11 +38,14 @@ describe('format rule ' + rule, function () {
     var document = parse(result);
 
     var links = document.querySelectorAll('link');
+    var styles = document.querySelectorAll('style');
     var scripts = document.querySelectorAll('script');
 
     it('should format well', function () {
         expect(links[0].hasAttribute('type')).toBe(false);
         expect(links[1].hasAttribute('type')).toBe(false);
+        expect(styles[0].hasAttribute('type')).toBe(false);
+        expect(styles[1].hasAttribute('type')).toBe(false);
         expect(scripts[0].hasAttribute('type')).toBe(false);
         expect(scripts[1].getAttribute('type')).toBe('text/plain');
         expect(scripts[2].hasAttribute('type')).toBe(false);
