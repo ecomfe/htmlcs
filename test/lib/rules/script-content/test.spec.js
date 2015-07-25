f1347e6d90534551a2acb6aa6bfc70c1fa769268
nighca <nighca@live.cn>
2015-07-25 15:22:36 +0800

    add indent info of node for script/style content lint

--- test/lib/rules/script-content/test.spec.js
+++ test/lib/rules/script-content/test.spec.js
@@ -16,11 +16,12 @@ describe('hint rule ' + rule, function () {
 
     var result = [];
 
-    var scriptLinter = function (content, pos, element) {
+    var scriptLinter = function (content, pos, element, indent) {
         result.push({
             content: content,
             pos: pos,
-            element: element
+            element: element,
+            indent: indent
         });
     };
 
@@ -46,15 +47,18 @@ describe('hint rule ' + rule, function () {
         expect(result[0].pos.line).toBe(1);
         expect(result[0].pos.col).toBe(9);
         expect(result[0].element.tagName).toBe('SCRIPT');
+        expect(result[0].indent).toBe('');
 
-        expect(result[1].content).toBe('\n    var a = 1;\n');
+        expect(result[1].content).toBe('\n\t    var a = 1;\n\t');
         expect(result[1].pos.line).toBe(2);
-        expect(result[1].pos.col).toBe(9);
+        expect(result[1].pos.col).toBe(10);
         expect(result[1].element.tagName).toBe('SCRIPT');
+        expect(result[1].indent).toBe('\t');
 
-        expect(result[2].content).toBe('\n    var a = {\n        index: 1\n    };\n');
+        expect(result[2].content).toBe('\n    var a = {\n        index: 1\n    };\n    ');
         expect(result[2].pos.line).toBe(5);
-        expect(result[2].pos.col).toBe(32);
+        expect(result[2].pos.col).toBe(36);
         expect(result[2].element.tagName).toBe('SCRIPT');
+        expect(result[2].indent).toBe('    ');
     });
 });
