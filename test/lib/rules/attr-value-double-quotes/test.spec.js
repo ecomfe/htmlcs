d0f6516f57dc3e3e3f0acca4b90a2a6fe777186d
nighca <nighca@live.cn>
2015-12-04 11:07:41 +0800

    test case for attr-value-double-quotes

--- test/lib/rules/attr-value-double-quotes/test.spec.js
+++ test/lib/rules/attr-value-double-quotes/test.spec.js
@@ -12,7 +12,7 @@ describe('hint rule ' + rule, function () {
     var result = htmlcs.hintFile(path.join(__dirname, 'case.html'));
 
     it('should return right result', function () {
-        expect(result.length).toBe(2);
+        expect(result.length).toBe(4);
 
         expect(result[0].type).toBe('WARN');
         expect(result[0].code).toBe('028');
@@ -23,5 +23,15 @@ describe('hint rule ' + rule, function () {
         expect(result[1].code).toBe('028');
         expect(result[1].line).toBe(12);
         expect(result[1].column).toBe(14);
+
+        expect(result[2].type).toBe('WARN');
+        expect(result[2].code).toBe('028');
+        expect(result[2].line).toBe(13);
+        expect(result[2].column).toBe(30);
+
+        expect(result[3].type).toBe('WARN');
+        expect(result[3].code).toBe('028');
+        expect(result[3].line).toBe(13);
+        expect(result[3].column).toBe(45);
     });
 });
