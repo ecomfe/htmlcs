7d5d4d1625478714dc24a0976d9d75022430e4d8
chris <wfsr@foxmail.com>
2016-07-09 00:37:52 +0800

    improve coverage of `ie-edge`

--- test/lib/rules/ie-edge/test.spec.js
+++ test/lib/rules/ie-edge/test.spec.js
@@ -14,6 +14,7 @@ describe('rule ' + rule, function () {
     var result2 = htmlcs.hintFile(path.join(__dirname, 'case2.html'));
     var result3 = htmlcs.hintFile(path.join(__dirname, 'case3.html'));
     var result4 = htmlcs.hintFile(path.join(__dirname, 'case4.html'));
+    var result5 = htmlcs.hintFile(path.join(__dirname, 'case5.html'));
 
     it('should return right result', function () {
         expect(result1.length).toBe(1);
@@ -30,6 +31,11 @@ describe('rule ' + rule, function () {
         expect(result4[0].code).toBe('011');
         expect(result4[0].line).toBe(3);
         expect(result4[0].column).toBe(1);
+
+        expect(result5[0].type).toBe('WARN');
+        expect(result5[0].code).toBe('011');
+        expect(result5[0].line).toBe(3);
+        expect(result5[0].column).toBe(1);
     });
 });
 
@@ -37,10 +43,12 @@ describe('format rule ' + rule, function () {
     var head1 = parse(htmlcs.formatFile(path.join(__dirname, 'case1.html'))).querySelector('head');
     var head2 = parse(htmlcs.formatFile(path.join(__dirname, 'case2.html'))).querySelector('head');
     var head4 = parse(htmlcs.formatFile(path.join(__dirname, 'case4.html'))).querySelector('head');
+    var head5 = parse(htmlcs.formatFile(path.join(__dirname, 'case5.html'))).querySelector('head');
 
     it('should format well', function () {
         expect(head1.querySelector('meta[http-equiv="X-UA-Compatible"]').getAttribute('content')).toBe('IE=Edge');
         expect(!head2).toBe(true);
         expect(head4.querySelector('meta[http-equiv="X-UA-Compatible"]').getAttribute('content')).toBe('IE=Edge');
+        expect(head5.querySelector('meta[http-equiv="X-UA-Compatible"]').getAttribute('content')).toBe('IE=Edge');
     });
 });
