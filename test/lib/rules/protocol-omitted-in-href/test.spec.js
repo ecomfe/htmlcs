51eec9f0264a2ecf5416db84670b70572a795fe3
Oleg Krivtsov <zexspectrum@gmail.com>
2017-04-10 15:31:45 +0700

    Fixed: Maybe it will be better to use false as default value for these two rules, considering backward compatibility. Fixed: Seems case for href="http://..." is missing.

--- test/lib/rules/protocol-omitted-in-href/test.spec.js
+++ test/lib/rules/protocol-omitted-in-href/test.spec.js
@@ -13,13 +13,17 @@ describe('hint rule ' + rule, function () {
     var result = htmlcs.hintFile(path.join(__dirname, 'case.html'));
 
     it('should return right result', function () {
-        expect(result.length).toBe(1);
+        expect(result.length).toBe(2);
 
         expect(result[0].type).toBe('WARN');
         expect(result[0].code).toBe('028');
         expect(result[0].line).toBe(8);
         expect(result[0].column).toBe(5);
 
+        expect(result[1].type).toBe('WARN');
+        expect(result[1].code).toBe('028');
+        expect(result[1].line).toBe(9);
+        expect(result[1].column).toBe(5);
     });
 });
 
