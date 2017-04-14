30babc72955e919923278bb6758a1679b62398f6
Oleg Krivtsov <zexspectrum@gmail.com>
2017-04-14 11:02:18 +0700

    Fixing: New rule - Use a new line for every block, list, or table element, and indent every such child element. #63

--- test/lib/rules/new-line-for-blocks/test.spec.js
+++ test/lib/rules/new-line-for-blocks/test.spec.js
@@ -13,12 +13,17 @@ describe('hint rule ' + rule, function () {
     var result = htmlcs.hintFile(path.join(__dirname, 'case.html'));
 
     it('should return right result', function () {
-        expect(result.length).toBe(1);
+        expect(result.length).toBe(2);
 
         expect(result[0].type).toBe('WARN');
         expect(result[0].code).toBe('028');
         expect(result[0].line).toBe(17);
         expect(result[0].column).toBe(24);
+        
+        expect(result[1].type).toBe('WARN');
+        expect(result[1].code).toBe('028');
+        expect(result[1].line).toBe(19);
+        expect(result[1].column).toBe(9);
 
     });
 });
