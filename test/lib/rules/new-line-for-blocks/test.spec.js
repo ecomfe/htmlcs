fa72147bdae04ef192a968150460f52b40fffe6d
Oleg Krivtsov <zexspectrum@gmail.com>
2017-05-10 10:26:28 +0700

    Fixed: 028 should be unique in all rules, maybe 049?

--- test/lib/rules/new-line-for-blocks/test.spec.js
+++ test/lib/rules/new-line-for-blocks/test.spec.js
@@ -16,17 +16,17 @@ describe('hint rule ' + rule, function () {
         expect(result.length).toBe(3);
 
         expect(result[0].type).toBe('WARN');
-        expect(result[0].code).toBe('028');
+        expect(result[0].code).toBe('049');
         expect(result[0].line).toBe(17);
         expect(result[0].column).toBe(24);
 
         expect(result[1].type).toBe('WARN');
-        expect(result[1].code).toBe('028');
+        expect(result[1].code).toBe('049');
         expect(result[1].line).toBe(17);
         expect(result[1].column).toBe(24);
 
         expect(result[2].type).toBe('WARN');
-        expect(result[2].code).toBe('028');
+        expect(result[2].code).toBe('049');
         expect(result[2].line).toBe(19);
         expect(result[2].column).toBe(5);
 
