89222c81198105c4fabffa6290d924ba2a5298c8
Oleg Krivtsov <zexspectrum@gmail.com>
2017-05-10 10:29:47 +0700

    Fixed: 050?

--- test/lib/rules/no-space-before-tag-end/test.spec.js
+++ test/lib/rules/no-space-before-tag-end/test.spec.js
@@ -16,7 +16,7 @@ describe('hint rule ' + rule, function () {
         expect(result.length).toBe(1);
 
         expect(result[0].type).toBe('WARN');
-        expect(result[0].code).toBe('028');
+        expect(result[0].code).toBe('050');
         expect(result[0].line).toBe(4);
         expect(result[0].column).toBe(26);
 
