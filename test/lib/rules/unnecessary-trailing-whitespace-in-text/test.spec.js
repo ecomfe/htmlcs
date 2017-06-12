38266ddbb53bcbbe40c8bd0ea7ef7919b60eeb92
Oleg Krivtsov <zexspectrum@gmail.com>
2017-06-12 14:51:27 +0700

    Fixing: New/Update Rule - Warn on excessive whitespace #92

--- test/lib/rules/unnecessary-trailing-whitespace-in-text/test.spec.js
+++ test/lib/rules/unnecessary-trailing-whitespace-in-text/test.spec.js
@@ -19,7 +19,7 @@ describe('hint rule ' + rule, function () {
         expect(result[0].code).toBe('052');
         expect(result[0].line).toBe(11);
         expect(result[0].column).toBe(11);
-        
+
         expect(result[1].type).toBe('WARN');
         expect(result[1].code).toBe('052');
         expect(result[1].line).toBe(12);
